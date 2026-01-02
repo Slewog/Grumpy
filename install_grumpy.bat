bf6f0d2d6dc3e5116be63ef9f46af3fe65fcb498
Slewog <31140215+Slewog@users.noreply.github.com>
2026-01-02 04:37:05 +0100

    Add win install script

---
+++ install_grumpy.bat
@@ -0,0 +1,20 @@
+@echo off
+set env_name=grumpy_env
+rem cd ..\
+
+
+
+@echo Creation of Grumpy Virtual Environment
+python -m venv %env_name%
+
+@echo Grumpy Virtual Environments successfully created
+@echo Next step: installing dependencies
+
+call .\%env_name%\Scripts\activate.bat
+python -m pip install --upgrade pip
+pip install -r requirements.txt
+call .\%env_name%\Scripts\deactivate.bat
+
+@echo Grumpy dependencies successfully installed and updated
+
+@pause
