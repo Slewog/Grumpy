aacbe05e66bd2c06decd2c956c7e781630be7500
Slewog <31140215+Slewog@users.noreply.github.com>
2026-01-03 02:09:45 +0100

    Add log on_ready event

--- src/bot.py
+++ src/bot.py
@@ -1,24 +1,18 @@
 import logging
 from pathlib import Path
 from dataclasses import dataclass
+import discord
 from discord.ext import commands
 
 from src.services.logging import build_logging
 from src.configs import Settings, get_settings, build_intents
 
-@dataclass(slots=True)
-class GrumpyApp:
-    settings: Settings
-    bot: commands.Bot
-
-    def run(self) -> None:
-        self.bot.run(token=self.settings.token)
-
 
 class Grumpy(commands.Bot):
-    def __init__(self, *, settings: Settings, logger: logging.Logger) -> None:
+    def __init__(self, *, settings: Settings) -> None:
         intents = build_intents()
-        self._logger = logger
+        self._logger = logging.getLogger('grumpy')
+        self.settings = settings
         super().__init__(
             command_prefix=settings.command_prefix,
             intents=intents,
@@ -26,21 +20,40 @@ class Grumpy(commands.Bot):
         )
 
     async def setup_hook(self) -> None:
-        print("Setting up bot...")
+        self._logger.info("Setting up bot...")
         # Load cogs here
 
     async def on_ready(self) -> None:
         """Called when the bot is ready."""
-        print(f"Logged in as {self.user} (ID: {self.user.id})")
+        all_guilds = self.guilds
+        guilds_count = len(all_guilds)
+
+        self._logger.info("Sucessfully logged as %s (CLIENT ID: %s) in %s Guilds", self.user, self.user.id, guilds_count)
+
+        for idx, guild in enumerate(all_guilds, start=1):
+            self._logger.debug("Connected on guilds %s/%s (NAME:%s, ID: %s, OWNER: %s, OWNER ID: %s)", idx, guilds_count, guild.name, guild.id, guild.owner, guild.owner_id)
+
         # Set commands translation here
+
         # Sync commands here
+
+        await self.change_presence(activity=discord.Game(name="Under development"), status=discord.Status.dnd) 
+
+
+@dataclass(slots=True)
+class GrumpyApp:
+    settings: Settings
+    bot: Grumpy
+
+    def run(self) -> None:
+        self.bot.run(token=self.settings.token)
+
 
 def create_bot() -> GrumpyApp:
     base_dir = Path(__file__).resolve().parent.parent
     build_logging(base_dir)
-    logger = logging.getLogger('grumpy')
 
     settings = get_settings(logger=logging.getLogger('grumpy.settings'), base_dir=base_dir)
 
-    bot = Grumpy(settings=settings, logger=logger)
+    bot = Grumpy(settings=settings)
     return GrumpyApp(settings=settings, bot=bot)
