2413b9d6975063236a722bfe26864b38ea577b2e
Slewog <31140215+Slewog@users.noreply.github.com>
2026-01-04 04:11:29 +0100

    Create bot_events.py

---
+++ src/bot_events.py
@@ -0,0 +1,40 @@
+from logging import getLogger
+from sys import exit as sys_exit
+from typing import TYPE_CHECKING
+from os import listdir as os_listdir
+from discord import Object, HTTPException
+
+from src.cogs import Admin, Owner
+
+if TYPE_CHECKING:
+    from src.bot import Grumpy
+
+async def register_cogs(bot: Grumpy):
+    logger = getLogger('grumpy.cogs')
+
+    logger.info("Loading of the 2 cogs has begun.")
+    await bot.add_cog(Admin(bot))
+    await bot.add_cog(Owner(bot))
+
+
+async def register_commands(bot: Grumpy):
+    """
+    Sync all comands global or for developement guild.
+
+    :param is_dev: if set to True commands will be sync  one guild.
+    """
+    test_guild_id = bot.settings.test_guild_id
+    logger = getLogger('grumpy.cogs')
+    synced: list = []
+
+    try:
+        if bot.is_dev_mode and test_guild_id:
+            guild = Object(id=test_guild_id)
+            bot.tree.copy_global_to(guild=guild)
+            synced = await bot.tree.sync(guild=guild)
+            logger.info("%s slash commands synchronized on the server %s for development", len(synced), test_guild_id)
+        else:
+            synced = await bot.tree.sync()
+            logger.info("%s slash commands are synchronized globally (may take some time).", len(synced))
+    except HTTPException as exc:
+        logger.error("Command synchronization failed: %s", exc)
