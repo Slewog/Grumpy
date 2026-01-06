1436b42e3dc71d24a5316746310339a0d147f9a0
Slewog <31140215+Slewog@users.noreply.github.com>
2026-01-06 14:15:32 +0100

    refactoring

--- src/configs/settings.py
+++ src/configs/settings.py
@@ -1,6 +1,6 @@
 from os import getenv
 from pathlib import Path
-from typing import Optional
+from typing import Optional, Literal
 from dotenv import load_dotenv
 from logging import Logger, DEBUG
 from dataclasses import dataclass
@@ -26,12 +26,6 @@ class Settings:
 
 
 def get_settings_from_json(json_file: Path):
-    """
-    :param json_file: grumpy.json dir path
-    :type json_file: Path
-    :param logger: Bot logger system
-    :type logger: Logger
-    """
     if not json_file.is_file():
         raise RuntimeError(f"Unable to access the settings files in {SETTING_FILE}, the program will shut down automatically.")
 
@@ -39,12 +33,6 @@ def get_settings_from_json(json_file: Path):
 
 
 def get_env_file_path(base_dir: Path, logger: Logger) -> Path:
-    """
-    :param base_dir: Current bot directory path
-    :type base_dir: str
-    :param logger: Bot logger system
-    :type logger: Logger
-    """
     env_file_path = base_dir / ENV_FILE_NAME
 
     if not env_file_path.is_file():
@@ -54,50 +42,32 @@ def get_env_file_path(base_dir: Path, logger: Logger) -> Path:
     return env_file_path
 
 
-def check_and_convert_link(raw_link: str, logger: Logger) -> str:
-    """
-    :param raw_link: Bot invitation link
-    :type raw_link: str
-    :param logger: Bot logger system
-    :type logger: Logger
-    """
+def check_and_convert_link(raw_invite_link: str, logger: Logger) -> str:
     BOT_LINK_INVITE_PATTERN = "https://discord.com/oauth2/authorize"
 
-    if raw_link == BOT_LINK_INVITE_DEFAULT or not BOT_LINK_INVITE_PATTERN in raw_link:
+    if raw_invite_link == BOT_LINK_INVITE_DEFAULT or not BOT_LINK_INVITE_PATTERN in raw_invite_link:
         logger.error("'INVITE_LINK' is nor set. Please verify that it is correctly defined in the %s file", ENV_FILE_NAME)
         raise RuntimeError(f"An error has been detected in the settings, the program will shut down automatically.")
 
-    return raw_link
+    return raw_invite_link
 
 
-def check_and_convert_token(raw_token: str, logger: Logger) -> str:
-    """
-    :param raw_token: Bot discord token
-    :type raw_token: str
-    :param logger: Bot logger system
-    :type logger: Logger
-    """
-    if raw_token == BOT_TOKEN_DEFAULT:
+def check_and_convert_token(raw_bot_token: str, logger: Logger) -> str:
+    if raw_bot_token == BOT_TOKEN_DEFAULT:
         logger.error("'DISCORD_BOT_TOKEN' is nor set and it's needed to launch the bot, please verify that it is correctly defined in the %s file", ENV_FILE_NAME)
         raise RuntimeError(f"An error has been detected in the settings, the program will shut down automatically.")
 
-    return raw_token
+    return raw_bot_token
 
 
-def convert_test_guild_id(raw_id: str, logger: Logger) -> int | None:
-    """
-    :param raw_id: GUILD ID FOR DEVELOPMENT
-    :type raw_id: str
-    :param logger: Bot logger system
-    :type logger: Logger
-    """
-    if raw_id == BOT_TEST_GUILD_ID:
+def convert_test_guild_id(raw_guild_id: str, logger: Logger) -> int | None:
+    if raw_guild_id == BOT_TEST_GUILD_ID:
         logger.warning("'TEST_GUILD_ID' is nor set and it's needed to sync commands to a guild on DEVELOPMENT Mode. Please verify that it is correctly defined in the %s file.", ENV_FILE_NAME)
         return None
 
     valid_id = None
     try:
-        valid_id = int(raw_id)
+        valid_id = int(raw_guild_id)
     except (ValueError, TypeError):
         logger.warning("Unable to convert 'TEST_GUILD_ID' it's needed to sync commands to a guild on DEVELOPMENT Mode. Please verify that it is correctly defined in the %s file.", ENV_FILE_NAME)
         return None
@@ -105,13 +75,7 @@ def convert_test_guild_id(raw_id: str, logger: Logger) -> int | None:
     return valid_id
 
 
-def check_status(raw_status: str, logger: Logger) -> str:
-    """
-    :param raw_status: bot status
-    :type raw_status: str
-    :param logger: Bot logger system
-    :type logger: Logger
-    """
+def check_status(raw_status: Literal["online", "offline", "idle", "dnd", "do_not_disturb", "invisible"], logger: Logger) -> str:
     BOT_STATUS = ["online", "offline", "idle", "dnd", "do_not_disturb", "invisible"]
 
     if not raw_status in BOT_STATUS:
@@ -120,11 +84,7 @@ def check_status(raw_status: str, logger: Logger) -> str:
     return raw_status
 
 
-def is_development(is_dev: str) -> bool:
-    """
-    :param is_dev: 'True' or 'False'
-    :type is_dev: str
-    """
+def is_development(is_dev: Literal["True", "False"]) -> bool:
     capitalized = is_dev.capitalize()
     if capitalized in ["True", "False"]:
         return bool(capitalized)
@@ -133,13 +93,6 @@ def is_development(is_dev: str) -> bool:
 
 
 def get_settings(base_dir: Path, logger: Logger) -> Settings:
-    """
-    :param base_dir: Current bot directory path
-    :type base_dir: Path
-    :param logger: Bot logger system
-    :type logger: Logger
-    """
-
     settings_dict = get_settings_from_json(base_dir / SETTING_FILE)
     logger.info("Successfully loaded settings from '%s'", SETTING_FILE)
 
