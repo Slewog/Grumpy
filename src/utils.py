5ac0c1388c545e0ba01de2a0895b29aa7166022d
Slewog <31140215+Slewog@users.noreply.github.com>
2026-01-07 13:44:35 +0100

    Add get_current_time func

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import datetime
 from typing import Any
 from pathlib import Path
 from discord import Status
@@ -13,6 +14,11 @@ BOT_VALID_STATUS = {
 }
 
 
+def get_current_time() -> str:
+    current_time = datetime.datetime.now()
+    return current_time.strftime("%d %b %Y at %H:%M:%S")
+
+
 def load_json_file(json_file: Path) -> dict[str, Any]:
     tmp_dict = {}
 
@@ -20,8 +26,8 @@ def load_json_file(json_file: Path) -> dict[str, Any]:
         with json_file.open("r", encoding="utf-8-sig") as fp:
             tmp_dict = json.load(fp)
     except json.JSONDecodeError:
-        raise RuntimeWarning(f"JSON FILE ERROR - Unable to decode the file: '{json_file}'")
+        raise RuntimeWarning(f"JSON FILE ERROR - Unable to decode the file: '{json_file}'.")
     except FileNotFoundError:
-        raise RuntimeError(f"JSON FILE ERROR - File not found: '{json_file}'")
+        raise RuntimeError(f"JSON FILE ERROR - File not found: '{json_file}'.")
 
     return tmp_dict
