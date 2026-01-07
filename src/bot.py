ad90f44717bb933e31a28b81e348c29febaaebaf
Slewog <31140215+Slewog@users.noreply.github.com>
2026-01-07 13:46:17 +0100

    Update logging, add event
    - Add guild add/remove event.
    - Add event when bot goes offline.
    - Add event when user join or leave a guild

--- src/bot.py
+++ src/bot.py
@@ -8,7 +8,7 @@ from discord.ext import commands
 from src.utils import BOT_VALID_STATUS
 from src.services import build_logging, CommandsTranslator
 from src.configs import build_intents, get_settings, Settings
-from src.bot_events import register_cogs, register_commands
+from src.bot_events import register_cogs, synchronize_commands
 
 class Grumpy(commands.Bot):
     def __init__(self, settings: Settings, intents: discord.Intents, logger: Logger) -> None:
@@ -25,7 +25,7 @@ class Grumpy(commands.Bot):
             command_prefix= commands.when_mentioned_or(settings.command_prefix),
             intents= intents
         )
-        self.log("Grumpy has been initialized")
+        self.log("Grumpy has been initialized.")
 
     async def setup_hook(self) -> None:
         await register_cogs(self)
@@ -35,23 +35,40 @@ class Grumpy(commands.Bot):
         guilds_count = len(all_guilds)
         is_development = self.is_development()
 
-        self.log("Sucessfully logged as %s (CLIENT ID: %s) in %s Guilds", self.user, self.user.id, guilds_count)
+        self.log("Sucessfully logged as %s (CLIENT ID: %s) in %s Guilds.", self.user, self.user.id, guilds_count)
 
         if is_development:
             for idx, guild in enumerate(all_guilds, start=1):
-                self.log("Connected on guilds %s/%s (NAME:%s, ID: %s, OWNER: %s, OWNER ID: %s)", idx, guilds_count, guild.name, guild.id, guild.owner, guild.owner_id, level="DEBUG")
+                self.log("Connected on guilds %s/%s (NAME:%s, ID: %s, OWNER: %s, OWNER ID: %s).", idx, guilds_count, guild.name, guild.id, guild.owner, guild.owner_id, level="DEBUG")
 
         await self.tree.set_translator(CommandsTranslator())
-        self.log("Commands translations system has been loaded")
+        self.log("Commands translations system has been loaded.")
 
-        await register_commands(self)
+        await synchronize_commands(self)
 
         await self.change_presence(
             activity= discord.Game(name= "Under development" if is_development else self.settings.activity),
             status= discord.Status.dnd if is_development else BOT_VALID_STATUS[self.settings.status]
         )
 
-    def get_test_guild_id(self):
+        self.log("Grumpy is ready to use.")
+
+    async def on_guild_join(self, guild: discord.Guild) -> None:
+        print(f'Joined guild: {guild.name} (ID: {guild.id})')
+
+    async def on_guild_remove(self, guild: discord.Guild) -> None:
+        print(f'Removed from guild: {guild.name} (ID: {guild.id})')
+
+    async def on_member_join(self, member: discord.Member) -> None:
+        pass
+
+    async def on_member_remove(self, member: discord.Member) -> None:
+        pass
+
+    async def on_disconnect(self) -> None:
+        self.log("Bot disconnexion requested by the owner.", level="WARNING")
+
+    def get_test_guild_id(self) -> int | None:
         return self.settings.test_guild_id
 
     def is_development(self) -> bool:
