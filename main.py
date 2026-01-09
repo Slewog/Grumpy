112bc80f0db9e40661f506ac62e2b8963a3b00e9
Slewog <31140215+Slewog@users.noreply.github.com>
2026-01-09 08:13:06 +0100

    Add new error catch

--- main.py
+++ main.py
@@ -1,20 +1,28 @@
 import sys
 
-from src import create_bot
+from src.bot import create_bot
+
+INPUT_MSG = "GRUMPY - Press Enter to close the program"
+
+def print_error(exc: str):
+    print(exc, file=sys.stderr)
+    input(INPUT_MSG)
 
 
 def main() -> int:
     try:
         app = create_bot()
-    except RuntimeError as exc:
-        print(str(exc), file=sys.stderr)
-        input("GRUMPY - Press Enter to close the program")
+    except (RuntimeError, FileNotFoundError, ValueError) as exc:
+        print_error(str(exc))
         return 1
 
     try:
         app.run()
     except KeyboardInterrupt:
         return 130
+    except FileNotFoundError as exc:
+        print_error(str(exc))
+        return 1
 
     return 0
 
